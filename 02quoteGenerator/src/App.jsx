ec6943648796f1ad1a81662b8e27c45647b98ef6
VibhutinandSingh <vibhutinandsingh@gmail.com>
2026-07-03 13:06:07 +0530

    add quotes and design basic structure

--- 02quoteGenerator/src/App.jsx
+++ 02quoteGenerator/src/App.jsx
@@ -1,120 +1,70 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const quotes = [
+    {
+      text: "Click generate to generate quotes",
+      author: "Dev"
+    },
+    {
+      text: "The best way to learn programming is by building projects.",
+      author: "Anonymous"
+    },
+    {
+      text: "Programs must be written for people to read.",
+      author: "Harold Abelson"
+    },
+    {
+      text: "Talk is cheap. Show me the code.",
+      author: "Linus Torvalds"
+    },
+    {
+      text: "Be yourself; everyone else is already taken.",
+      author: "Oscar Wilde "
+    },
+    {
+      text: "A friend is someone who knows all about you and still loves you.",
+      author: "Elbert Hubbard"
+    },
+    {
+      text: "Always forgive your enemies; nothing annoys them so much.",
+      author: "Oscar Wilde "
+    },
+    {
+      text: "We accept the love we think we deserve.",
+      author: " Stephen Chbosky, The Perks of Being a Wallflower "
+    },
+    {
+      text: "Without music, life would be a mistake",
+      author: " Friedrich Nietzsche, Twilight of the Idols "
+    },
+    {
+      text: "All that glitters is not gold.",
+      author: "William Shakespeare"
+    },
+    {
+      text: "Eighty percent of success is showing up.",
+      author: "Woody Allen"
+    },
+    {
+      text: "A rose by any other name would smell as sweet.",
+      author: "William Shakespeare"
+    },
+  ];
+
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
+      <div className='flex flex-col gap-10  justify-center items-center'>
+        <h1>Quote Generator</h1>
+        <div className='bg-gray-500 text-white w-230 h-30 flex flex-col justify-center items-center gap-4  rounded-2xl'>
+
         </div>
         <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+          className='bg-red-500 text-white p-4 m-2  rounded-3xl hover:scale-110 hover:bg-red-800 ease-in duration-100 '>Generate</button>
+      </div>
     </>
   )
 }
